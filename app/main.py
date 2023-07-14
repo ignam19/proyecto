869047537d3cf0651f6df85051c7f7ac56b7e491
ignam19 <ignaciomersch602811@gmail.com>
2023-07-14 12:44:14 -0300

    tabla en frame y todo en place

--- app/main.py
+++ app/main.py
@@ -8,7 +8,9 @@ ventana.iconbitmap("C:\python-vscode\proyecto\\app\icono.ico")
 
 #entrada
 e_texto = Entry(ventana, font= ("Calibri 15"))
-e_texto.grid(row= 0, column= 0, columnspan= 4, padx= 5, pady= 5)
+
+#agregar entrada
+e_texto.place(x= 5, y= 5,)
 
 
 #botones
@@ -16,16 +18,20 @@ boton_buscar = Button(ventana, text= "Buscar", width= 6, height= 1)
 
 
 #agregar botones
-boton_buscar.grid(row= 0, column= 4, padx= 5, pady= 5)
+boton_buscar.place(x= 220, y= 5)
+
+#frame tabla
+frame_tabla= Frame(ventana)
+frame_tabla.place(x= 5, y= 30)
 
 #tabla
-tabla = ttk.Treeview(ventana, columns=("column1", "column2", "column3"))
+tabla = ttk.Treeview(frame_tabla, columns=("column1", "column2", "column3"))
 
 #estilo de columnas
-tabla.column("#0", width= 80)
-tabla.column("column1", width= 80, anchor= CENTER)
-tabla.column("column2", width= 80, anchor= CENTER)
-tabla.column("column3", width= 80, anchor= CENTER)
+tabla.column("#0", width= 220)
+tabla.column("column1", width= 220, anchor= CENTER)
+tabla.column("column2", width= 220, anchor= CENTER)
+tabla.column("column3", width= 220, anchor= CENTER)
 
 #estilo de cabecera de columnas
 tabla.heading("#0", text= "Código", anchor= CENTER)
@@ -34,7 +40,7 @@ tabla.heading("column2", text= "Stock", anchor= CENTER)
 tabla.heading("column3", text= "Precio", anchor= CENTER)
 
 #insercion de tabla
-tabla.grid(row= 1, column= 0, columnspan= 5, padx= 5, pady= 5)
+tabla.grid(row= 0, column= 0, columnspan= 5, pady= 5)
 
 
 
